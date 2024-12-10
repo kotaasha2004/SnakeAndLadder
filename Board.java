bb18d4d607b4578a8e035e4683f6b72767245e64
kotaasha2004 <168574635+kotaasha2004@users.noreply.github.com>
2024-12-11 00:13:18 +0530

    Create Board.java

---
+++ Board.java
@@ -0,0 +1,20 @@
+public class Board{
+  private int size;
+  private int start;
+  private int end;
+  public Board(int size){
+    this.start=1;
+    this.end=start+size-1;
+    this.size=size;
+  }
+  public int getStart(){
+    get start;
+  }
+  public int getSize(){
+    get size;
+  }
+  public int getEnd(){
+    return end;
+  }
+}
+  
