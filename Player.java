81c4ac6bf06b3e52caf0b665ce4ed6ff32f8b6c5
kotaasha2004 <168574635+kotaasha2004@users.noreply.github.com>
2024-12-10 23:47:27 +0530

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,22 @@
+public class Player{
+  private String name;
+  private int position;
+  private boolean won;
+  public Player(String name){
+    this.name=name;
+    this.won=false;
+  }
+  public int getPosition(){
+    return position;
+  }
+  public String getName(){
+    return name;
+  }
+  public void setPosition(int position2){
+    position=position1;
+  }
+   public void setWon(boolean b){
+    won=true;
+  }
+  
+  
