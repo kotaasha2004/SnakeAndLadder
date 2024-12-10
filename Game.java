3a1c8587afb97a4105636ed22956da077348fd79
kotaasha2004 <168574635+kotaasha2004@users.noreply.github.com>
2024-12-11 00:49:06 +0530

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,1 +1,22 @@
-public class Game
+public class Game{
+  private int numberOfSnakes;
+  private int numberOfLadders;
+
+  private Queue<Player> players;
+  private List<Snake> snakes;
+  private List<Ladder> ladder;
+
+  private Board board;
+  private Dice dice;
+  public Game(int numberOfLadders, int numberOfSnakes,int boardSize){
+   this.numberOfSnakes=numberOfSnakes;
+   this.numberOfLadders=numberOfLadders;
+    this.Players=new ArrayDeque<>();
+    snakes=new ArrayList<>(numberOfSnakes);
+    ladders=new ArrayList<>(numberOfLadders);
+    board=new Board(boardSize);
+    dice=new Dice();
+    initBoard();
+    
+  }
+  
